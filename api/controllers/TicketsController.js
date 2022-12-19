f2feadd7b5620adac0b9d5c5e962dc57879b877c
Fabian Hide <graadev@gmail.com>
2022-12-19 02:02:34 -0600

    idk

--- api/controllers/TicketsController.js
+++ api/controllers/TicketsController.js
@@ -11,28 +11,25 @@ export const getAllTickets = async (req, res) => {
 }
 
 export const addTicket = async (req, res) => {
+    const poolCon = await pool.getConnection()
     try {
+        await poolCon.beginTransaction()
 
-        /*
-        Example
-        [{folio: 123, tipoVale: 'Salida', other keys and values }, [{insumo: 'Rescate', cantidad: 5}, {insumo: 'Malation', cantidad: 2}]]
-        result in db should be
-        folio = 123, tipoVale = Salida, Insumo = Rescate, Cantidad = 5
-        folio = 123, tipoVale = Salida, Insumo = Malation, Cantidad = 2
-        */
         const { folio, tipoVale, descripcion, observacion, entrego, recibio, fecha } = req.body[0]
         for (let index = 0; index < req.body[1].length; index++) {
-            await pool.execute('insert into vales (folio, tipoVale, descripcion, observacion, entrego, recibio, fecha, codigoInsumo, cantidad) values (?,?,?,?,?,?,?,?,?)',
+            await poolCon.execute('insert into vales (folio, tipoVale, descripcion, observacion, entrego, recibio, fecha, codigoInsumo, cantidad) values (?,?,?,?,?,?,?,?,?)',
                 [folio, tipoVale, descripcion, observacion, entrego, recibio, fecha, req.body[1][index].insumo, req.body[1][index].cantidad])
             if (tipoVale === 'Entrada') {
-                await pool.execute(`update productos set cantidad = cantidad + ${req.body[1][index].cantidad} where nombre = '${req.body[1][index].insumo}'`)
+                await poolCon.execute(`update productos set cantidad = cantidad + ${req.body[1][index].cantidad} where nombre = '${req.body[1][index].insumo}'`)
             } else {
-                await pool.execute(`update productos set cantidad = cantidad - ${req.body[1][index].cantidad} where nombre = '${req.body[1][index].insumo}'`)
+                await poolCon.execute(`update productos set cantidad = cantidad - ${req.body[1][index].cantidad} where nombre = '${req.body[1][index].insumo}'`)
             }
         }
-
+        await poolCon.commit()
+        poolCon.release()
         res.status(200).json()
     } catch (error) {
+        await poolCon.rollback()
         res.status(404).json({ message: error.message })
     }
 }
@@ -48,8 +45,8 @@ export const editTicket = async (req, res) => {
 export const deleteTicket = async (req, res) => {
     const { idDelete } = req.params
     try {
-        const ticket =  await pool.query('select * from vales where idVale =' + idDelete)
-       const x = await pool.query('delete from vales where idProducto =' + idDelete)
+        const ticket = await pool.query('select * from vales where idVale =' + idDelete)
+        const x = await pool.query('delete from vales where idProducto =' + idDelete)
         res.status(200).json(x)
     } catch (error) {
         res.status(404).json({ message: error.message })
--- client/src/pages/Tickets.jsx
+++ client/src/pages/Tickets.jsx
@@ -12,12 +12,69 @@ import Delete from '../components/Modals/Delete'
 import { Toaster } from 'react-hot-toast'
 import FloatButton from 'antd/es/float-button'
 
+import { Badge, Dropdown, Row, Space, Table } from 'antd';
+import { merge } from 'antd/es/theme/util/statistic'
 
+const tableColumns = [
+  {
+    title: 'Folio',
+    dataIndex: 'folio',
+    key: 'folio',
+  },
+  {
+    title: 'Tipo Vale',
+    dataIndex: 'tipoVale',
+    key: 'tipoVale',
+  },
+  {
+    title: 'Descripcion',
+    dataIndex: 'descripcion',
+    key: 'descripcion',
+  },
+  {
+    title: 'Observacion',
+    dataIndex: 'observacion',
+    key: 'observacion',
+  },
+  {
+    title: 'Entrego',
+    dataIndex: 'entrego',
+    key: 'entrego',
+  },
+  {
+    title: 'Recibio',
+    dataIndex: 'recibio',
+    key: 'recibio',
+  },
+  {
+    title: 'Fecha',
+    dataIndex: 'fecha',
+    key: 'fecha',
+  },
+
+  {
+    title: 'Opciones',
+    key: 'opciones',
+    render: () => <a>Publish</a>,
+  },
+];
+
+const items = [
+  {
+    key: '1',
+    label: 'Action 1',
+  },
+  {
+    key: '2',
+    label: 'Action 2',
+  },
+];
 const Tickets = () => {
 
   const [allTickets, setAllTickets] = useState([])
   const [filteredTickes, setFilteredTickets] = useState([])
   const [loading, setLoading] = useState(true)
+  const [sepRow, setSepRow] = useState([])
   const [modals, setModals] = useState({
     add: { isOpen: false },
     edit: { isOpen: false, product: {} },
@@ -64,13 +121,69 @@ const Tickets = () => {
     }, [])
   }
 
+  const subTableColumns = [
+    {
+      title: 'Insumo',
+      dataIndex: 'insumo',
+      key: 'insumo',
+    },
+    {
+      title: 'Cantidad',
+      dataIndex: 'cantidad',
+      key: 'cantidad',
+    },
+    {
+      title: 'Opciones',
+      dataIndex: 'operation',
+      key: 'operation',
+      render: () => (
+        <Space size="middle">
+          <a>Pause</a>
+          <a>Stop</a>
+          <Dropdown
+            menu={{
+              items,
+            }}
+          >
+            <a>
+              More +
+            </a>
+          </Dropdown>
+        </Space>
+      ),
+    },
+  ];
+
+  const mergeArray = (arr) => {
+    return arr.reduce((acc, val) => {
+      const ind = acc.findIndex(item => item.folio === val.folio)
+      if (ind !== -1) {
+        acc[ind].insumos.push({ key: val.folio, insumo: val.codigoInsumo, cantidad: val.cantidad })
+
+      } else {
+        acc.push({
+          key: val.folio,
+          folio: val.folio,
+          tipoVale: val.tipoVale,
+          descripcion: val.descripcion,
+          observacion: val.observacion,
+          entrego: val.entrego,
+          recibio: val.recibio,
+          insumos: [{ key: val.folio, insumo: val.codigoInsumo, cantidad: val.cantidad }]
+        });
+      };
+      return acc
+    }, [])
+  }
+
   const fetchTickets = async () => {
     try {
       const response = await fetch('http://localhost:3000/vales')
       const data = await response.json()
-      console.log(data);
-      setAllTickets(editDeleteButton(data))
-      setFilteredTickets(editDeleteButton(data))
+      const newData = await mergeArray(data).map(ticket => ticket.insumos)
+      setSepRow(newData)
+      setAllTickets(mergeArray(data))
+      setFilteredTickets(mergeArray(data))
       setLoading(false)
     } catch (error) {
       console.log(error)
@@ -81,32 +194,39 @@ const Tickets = () => {
     fetchTickets()
   }, [])
 
-/* 
-  useEffect(() => {
-    if (filters.categoria === 'x') return setFilters(prev => ({ ...prev, categoria: '' }))
-    if (filters.unidad === 'x') return setFilters(prev => ({ ...prev, unidad: '' }))
-    if (filters.cantidad === 'x') return setFilters(prev => ({ ...prev, cantidad: '' }))
-
-    setFilteredProducts(allProducts.filter(product => {
-      for (const [key, value] of Object.entries(filters)) {
-        if (key === 'codigo' || key === 'nombre') {
-          if (value !== "" && !product[key].toLocaleLowerCase().includes(value.toLocaleLowerCase())) return false;
-        } else if (key === 'cantidad') {
-          if (value !== "" && !(product[key] >= value.split('-')[0] && product[key] <= value.split('-')[1])) return false;
-        } else {
-          if (value !== "" && !product[key].includes(value)) return false;
+  /* 
+    useEffect(() => {
+      if (filters.categoria === 'x') return setFilters(prev => ({ ...prev, categoria: '' }))
+      if (filters.unidad === 'x') return setFilters(prev => ({ ...prev, unidad: '' }))
+      if (filters.cantidad === 'x') return setFilters(prev => ({ ...prev, cantidad: '' }))
+  
+      setFilteredProducts(allProducts.filter(product => {
+        for (const [key, value] of Object.entries(filters)) {
+          if (key === 'codigo' || key === 'nombre') {
+            if (value !== "" && !product[key].toLocaleLowerCase().includes(value.toLocaleLowerCase())) return false;
+          } else if (key === 'cantidad') {
+            if (value !== "" && !(product[key] >= value.split('-')[0] && product[key] <= value.split('-')[1])) return false;
+          } else {
+            if (value !== "" && !product[key].includes(value)) return false;
+          }
         }
-      }
-      return true;
-    }))
-  }, [filters])
- */
+        return true;
+      }))
+    }, [filters])
+   */
+  const expandedRowRender = (row) => {
+    console.log(row);
+    const inTable = sepRow[Row.key]
+
+    return <Table columns={subTableColumns} dataSource={row.insumos} pagination={false} />;
+  }
+
 
   return (
     <Layout>
       <Toaster />
-      <Add modals={modals} setModals={setModals} fetchTickets={fetchTickets}/>
-    {/*  
+      <Add modals={modals} setModals={setModals} fetchTickets={fetchTickets} />
+      {/*  
       <Edit modals={modals} setModals={setModals} fetchProducts={fetchProducts}/>
       <Delete modals={modals} setModals={setModals} fetchProducts={fetchProducts} texto='El insumo se elimno con exito'/> */}
 
@@ -116,7 +236,7 @@ const Tickets = () => {
         <CategoryFilter setFilters={setFilters} filters={filters} />
         <QuantityFilter setFilters={setFilters} filters={filters} />
         <UnitFilter setFilters={setFilters} filters={filters} />
-        <button onClick={() => setModals(prev => ({ ...prev, add: { isOpen: true } }))} className='bg-indigo-200 hover:bg-indigo-400 font-semibold text-gray-700 hover:text-white rounded-lg px-4 py-2 min-w-fit w-full '>Agregar Vale</button>
+        <button onClick={() => console.log(sepRow)} className='bg-indigo-200 hover:bg-indigo-400 font-semibold text-gray-700 hover:text-white rounded-lg px-4 py-2 min-w-fit w-full '>Agregar Vale</button>
       </div>
       <div className='max-w-7xl mx-auto '>
         {
@@ -151,19 +271,19 @@ const Tickets = () => {
                       <TableRow texto={ticket.cantidad} />
                       {
                         ticket.options ? (<td className="py-3 px-6 text-center">
-                        <div className="flex item-center justify-center">
-                          <div
-                            className="w-4 mr-2 transform hover:text-blue-500 cursor-pointer hover:scale-110">
-                            <PencilSquareIcon onClick={() => setModals(prev => ({ ...prev, edit: {isOpen: true, product} }))} />
+                          <div className="flex item-center justify-center">
+                            <div
+                              className="w-4 mr-2 transform hover:text-blue-500 cursor-pointer hover:scale-110">
+                              <PencilSquareIcon onClick={() => setModals(prev => ({ ...prev, edit: { isOpen: true, product } }))} />
+                            </div>
+                            <div
+                              className="w-4 mr-2 transform hover:text-red-500 cursor-pointer hover:scale-110">
+                              <TrashIcon onClick={() => setModals(prev => ({ ...prev, delete: { isOpen: true, id: product.idProducto } }))} />
+                            </div>
                           </div>
-                          <div
-                            className="w-4 mr-2 transform hover:text-red-500 cursor-pointer hover:scale-110">
-                            <TrashIcon onClick={() => setModals(prev => ({ ...prev, delete: {isOpen: true, id: product.idProducto} }))} />
-                          </div>
-                        </div>
-                      </td>) : null
+                        </td>) : null
                       }
-                      
+
                     </tr>
                   ))
                 }
@@ -172,7 +292,21 @@ const Tickets = () => {
           )
         }
       </div>
-      <FloatButton.BackTop/>
+      <FloatButton.BackTop />
+      <div>
+        <p>a</p>
+        <Table
+          columns={tableColumns}
+          pagination={false}
+          expandable={{
+            expandedRowRender,
+
+
+          }}
+          dataSource={allTickets}
+        />
+        <p>a</p>
+      </div>
     </Layout>
   )
 }
